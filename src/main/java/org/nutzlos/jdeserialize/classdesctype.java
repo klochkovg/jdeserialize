bc92a9028426f7655d0d2443e1824f477439022d
Georgy Klochkov <klochkovg@gmail.com>
2024-09-01 12:33:08 +0200

    Initial commit, transfer of the old codebase

---
+++ src/main/java/org/nutzlos/jdeserialize/classdesctype.java
@@ -0,0 +1,8 @@
+package org.nutzlos.jdeserialize;
+
+/**
+ * Enum for class description types.
+ */
+public enum classdesctype {
+    NORMALCLASS, PROXYCLASS
+}
---
+++ src/main/java/org/nutzlos/jdeserialize/contenttype.java
@@ -0,0 +1,5 @@
+package org.nutzlos.jdeserialize;
+
+public enum contenttype {
+    INSTANCE, CLASS, ARRAY, STRING, ENUM, CLASSDESC, BLOCKDATA, EXCEPTIONSTATE
+}
